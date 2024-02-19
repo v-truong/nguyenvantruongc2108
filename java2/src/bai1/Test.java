eb3a814d07082c8c1904060ebca989d6f6230526
truong <truong.nv.1929@aptechlearning.edu.vn>
2024-02-19 16:30:39 +0700

    day code

--- java2/src/bai1/Test.java
+++ java2/src/bai1/Test.java
@@ -5,16 +5,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Test {
     List<Opportunity> Opp=new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        opportunities o =new opportunities();
-        
-        o.add();
-        o.writeFile();
-       
+        Random random = new Random();
+        int min = 100000;
+        int max = 999999;
+        int randomNumber = random.nextInt(max - min + 1) + min;
+        System.out.println(randomNumber);
+    }
 
     }
-   
-}
